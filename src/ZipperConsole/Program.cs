ed5fc622ae76841c35676bb6324b3d211a64fe7a
Sean Rafferty <seanr@randox.com>
2022-09-30 17:58:38 +0100

    git changes

--- src/ZipperConsole/Program.cs
+++ src/ZipperConsole/Program.cs
@@ -1,29 +1,23 @@
 ﻿using System.Runtime.InteropServices;
+using ZipperConsole;
 
-namespace ZipperConsole;
 
-public class Program
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Running: Zipper!");
+
+//Simple sleeper
+Thread.Sleep(500);
+
+string path = System.Runtime.InteropServices.RuntimeInformation
+                                        .IsOSPlatform(OSPlatform.Windows) ? "../AppFiles": "..//MacFiles";
+// If directory does not exist, don't even try   
+if(!Directory.Exists(path))
 {
-    public static void Main(string[] args)
-    {
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Running: Zipper!");
-
-        //Simple sleeper
-        Thread.Sleep(1500);
-
-        string path = System.Runtime.InteropServices.RuntimeInformation
-                                               .IsOSPlatform(OSPlatform.Windows) ? "../AppFiles": "/MacFiles";
-        // If directory does not exist, don't even try   
-        if(!Directory.Exists(path))
-        {
-            Console.WriteLine("Path is invalid");
-            Environment.Exit(1);
-        }
-
-        int count = FileHandler.getCountOfFilesInFolder(path);
-        Console.WriteLine($"A total of {count} files are available to compress");
-
-        Console.WriteLine("App Complete");
-    }
+    Console.WriteLine("Path is invalid");
+    Environment.Exit(1);
 }
+
+int count = FileHandler.getCountOfFilesInFolder(path);
+Console.WriteLine($"A total of {count} files are available to compress");
+
+Console.WriteLine("App Complete");
